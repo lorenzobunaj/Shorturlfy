d0f3e1f3cc8cc0c04dcf5d8895be62074fe33258
Lorenzo Bunaj <lorenzobunaj@gmail.com>
2023-03-17 17:22:59 +0100

    initial commit

---
+++ utils/conf.py
@@ -0,0 +1,4 @@
+CONF = {
+    "SECURE_KEY" : "your_secure_key", # the static key used to encrypt and decrypt the urls and refers
+    "DB_URI" : "your_db_uri" # the uri used to connect to mongodb database
+}
